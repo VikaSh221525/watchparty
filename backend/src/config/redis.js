a7995f1071112848332c375e14c68931ef6b5c6a
vikassharma221525@gmail.com <vikassharma221525@gmail.com>
2026-07-02 18:49:13 +0530

    Shifted to Upstash

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,6 +1,7 @@
 import Redis from 'ioredis';
 import { logger } from '../utils/logger.js';
 
+// Redis client instance and availability flag
 let redisClient = null;
 let isRedisAvailable = false;
 
